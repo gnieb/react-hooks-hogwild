1a53c66f4f87e1c3c6c1e9fee721ae8de75b9217
Grace Nieboer <grace.nieboer@outlook.com>
2023-02-24 11:01:27 -0500

    highest medal chieved

--- src/components/HogItem.js
+++ src/components/HogItem.js
@@ -5,20 +5,19 @@ console.log(hogs)
 
 function HogItem (hog) {
 
-
-
+    // const highestMedal = 'highest medal achieved'
+    // const highestMedalKey = hog[highestMedal]
+    // console.log(hog)
     return (
 
         <div className='pigTile'>
             <h3>{hog.name}</h3>
             <p>Specialty: {hog.specialty}</p>
             <p>Weight: {hog.weight}</p>
-            <p>Greased: {hog.greased}</p>
-            <p></p>
+            <p>Greased: {hog.greased.toString()} </p>
+            <p>Highest medal achieved: {hog.highestMedalAchieved} </p>
             <img src={hog.image} />
             
-
-
 
         </div>
     )
--- src/components/HogList.js
+++ src/components/HogList.js
@@ -6,8 +6,9 @@ import hogs from "../porkers_data.js"
 function HogList () {
 
 const displayHogList = hogs.map((hog) => {
+    console.log(hog['highest medal achieved'])
          return (
-         <HogItem name={hog.name} specialty={hog.specialty} weight={hog.weight} greased={hog.greased} image={hog.image}/>
+         <HogItem name={hog.name} specialty={hog.specialty} weight={hog.weight} greased={hog.greased} image={hog.image} highestMedalAchieved= {hog['highest medal achieved']} />
          )
     })
 
